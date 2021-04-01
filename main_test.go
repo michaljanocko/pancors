1a36bc168336023fe1451cf4f74e6e4db198c894
Michal Janočko <michal@janocko.com>
2021-04-01 14:56:25 -0500

    ✅ Added tests (in 2^5 Go code lines)

---
+++ main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestProxy(t *testing.T) {
+	go main()
+
+	url := "http://localhost:8080/?url=https%3A%2F%2Fsuggest.seznam.cz%2Fslovnik%2Fmix_cz_en%3Fphrase%3Dtest%26format%3Djson-2"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic("Couldn't create testing request")
+	}
+
+	req.Header.Add("origin", "test")
+
+	res, _ := http.DefaultClient.Do(req)
+	if err != nil {
+		panic("Couldn't fetch testing data")
+	}
+
+	defer res.Body.Close()
+
+	if res.Header.Get("Access-Control-Allow-Origin") != "*" {
+		panic("Access-Control-Allow-Origin is not '*'")
+	}
+	if res.Header.Get("Access-Control-Allow-Credentials") != "true" {
+		panic("Access-Control-Allow-Credentials is not 'true'")
+	}
+}
