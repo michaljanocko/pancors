e56ebe178507fce7ff9960f00026cdc739c718eb
Michal Janočko <michal@janocko.com>
2021-03-31 21:37:50 -0500

    ✨ It works!

--- main.go
+++ main.go
@@ -4,12 +4,61 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 )
+
+func getListenPort() string {
+	port, isSet := os.LookupEnv("PORT")
+
+	if !isSet {
+		return ":8080"
+	}
+
+	return ":" + port
+}
+
+func invalidUrl(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprint(w, "Invalid URL")
+}
+
+type CorsTransport http.Header
+
+func (t CorsTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	res, err := http.DefaultTransport.RoundTrip(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+		urlParam := r.URL.Query().Get("url")
+
+		urlParsed, err := url.Parse(urlParam)
+		if err != nil || (urlParsed.Scheme != "http" && urlParsed.Scheme != "https") {
+			invalidUrl(w)
+			return
+		}
+
+		headers := http.Header{}
+		headers.Set("Access-Control-Allow-Origin", "*")
+		headers.Set("Access-Control-Allow-Credentials", "true")
+
+		proxy := httputil.ReverseProxy{
+			Director: func(r *http.Request) {
+				r.URL = urlParsed
+				r.Host = urlParsed.Host
+			},
+			Transport: CorsTransport(headers),
+		}
+
+		proxy.ServeHTTP(w, r)
 	})
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(getListenPort(), nil))
 }
