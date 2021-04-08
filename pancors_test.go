7974b4b60fa792c7275d6b60fe9f60cc38c657cc
David Kuridža <david@kuridza.si>
2021-04-08 22:23:51 +0200

    Add more tests

--- pancors_test.go
+++ pancors_test.go
@@ -1,35 +1,103 @@
 package pancors
 
 import (
+	"context"
 	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
 func TestProxy(t *testing.T) {
-	go func() {
-		http.HandleFunc("/", HandleProxy)
-		panic(http.ListenAndServe(":8080", nil))
-	}()
+	ts := httptest.NewServer(http.HandlerFunc(HandleProxy))
+	defer ts.Close()
 
-	url := "http://localhost:8080/?url=https%3A%2F%2Fsuggest.seznam.cz%2Fslovnik%2Fmix_cz_en%3Fphrase%3Dtest%26format%3Djson-2"
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		panic("Couldn't create testing request")
+	type expected struct {
+		statusCode int
+		headers    map[string]string
 	}
 
-	req.Header.Add("origin", "test")
-
-	res, _ := http.DefaultClient.Do(req)
-	if err != nil {
-		panic("Couldn't fetch testing data")
+	tests := []struct {
+		name     string
+		url      string
+		expected expected
+	}{
+		{
+			"https url with params",
+			"https://suggest.seznam.cz/slovnik/mix_cz_en?phrase=test&format=json-2",
+			expected{
+				http.StatusOK,
+				map[string]string{
+					"Access-Control-Allow-Origin":      "*",
+					"Access-Control-Allow-Credentials": "true",
+				},
+			},
+		},
+		{
+			"http url with params",
+			"http://suggest.seznam.cz/slovnik/mix_cz_en?phrase=test&format=json-2",
+			expected{
+				http.StatusOK,
+				map[string]string{
+					"Access-Control-Allow-Origin":      "*",
+					"Access-Control-Allow-Credentials": "true",
+				},
+			},
+		},
+		{
+			"empty url",
+			"",
+			expected{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			"non http(s) url",
+			"ftp://example.com",
+			expected{
+				statusCode: http.StatusBadRequest,
+			},
+		},
 	}
 
-	defer res.Body.Close()
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			u, err := url.Parse(ts.URL)
+			if err != nil {
+				t.Logf("could not parse test server's url; got %v", err)
+				t.FailNow()
+			}
 
-	if res.Header.Get("Access-Control-Allow-Origin") != "*" {
-		panic("Access-Control-Allow-Origin is not '*'")
-	}
-	if res.Header.Get("Access-Control-Allow-Credentials") != "true" {
-		panic("Access-Control-Allow-Credentials is not 'true'")
+			q := u.Query()
+			q.Set("url", tc.url)
+			u.RawQuery = q.Encode()
+
+			req, err := http.NewRequestWithContext(context.Background(), "GET", u.String(), nil)
+			if err != nil {
+				t.Logf("could not prepare a request; got %v", err)
+				t.FailNow()
+			}
+
+			rsp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Log("could not fetch testing data")
+				t.FailNow()
+			}
+			defer rsp.Body.Close()
+
+			if rsp.StatusCode != tc.expected.statusCode {
+				t.Logf("expected HTTP status code %d; got %d", tc.expected.statusCode, rsp.StatusCode)
+				t.Fail()
+			}
+
+			for header, expected := range tc.expected.headers {
+				actual := rsp.Header.Get(header)
+				if actual != expected {
+					t.Logf("expected header %s = %s; got: %v", header, expected, actual)
+					t.Fail()
+				}
+			}
+		})
 	}
 }
